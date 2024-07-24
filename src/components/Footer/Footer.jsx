15270c7018d8d0d3e6e027c0f5adb8698918ac38
David <agodidavid004@gmail.com>
2024-07-24 09:39:17 +0100

    added number

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import { IconFacebook, IconInstagram, IconLinkedin, IconTwitter } from "../../as
 const Footer = () => {
   return (
     <div className="flex items-center sm:justify-between justify-center gap-5  flex-wrap md:px-20 px-10 py-10">
-        <h3>Contact Us: stemcamp@bt2spec.com</h3>
+        <h3 className="text-center">Contact Us: stemcamp@bt2spec.com || 08033653605</h3>
         <div className="flex items-center sm:gap-4 gap-2 text-white">
             <a href="https://facebook.com/bt2spec" target="blank" rel="noreferrer"><div className="flex items-center justify-center rounded-full border px-2 py-2 text-[17px] bg-[#3b5998]"><IconFacebook/></div></a>
             <a href="https://instagram.com/bt2spec" target="blank" rel="noreferrer"><div className="flex items-center justify-center rounded-full border px-2 py-2 text-[17px] bg-black"><IconInstagram/></div></a>
